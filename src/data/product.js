70c1b9e737a3ada6799cfaf5924b0f981608b30a
Lautaroalt <lautaroaltamirano@hotmail.com>
2023-11-15 21:05:54 -0300

    img

--- src/data/product.js
+++ src/data/product.js
@@ -1,28 +1,28 @@
 export const products= [
     {
     id: 1,
-    title: "Camisa a rayas",
-    description: "Camisa de manga larga con rayas elegantes",
-    price: 3000,
+    title: "Remera basica marron",
+    description: "Remera de algodon 100% con un estampado de excelente calidad",
+    price: 5500,
     category: "Hombre",
-    pictureUrl: "https://picsum.photos/200/300?random=1",
+    pictureUrl: "https://d368r8jqz0fwvm.cloudfront.net/27252-product_lg/remera-de-hombre-marko.jpg",
   },
 
     {
     id: 2,
-    title: "Vestido floral",
-    description: "Vestido corto con estampado floral",
-    price: 3999,
+    title: "Remera Oversized Silueta",
+    description: "Remera de algodon 100% con un estampado de excelente calidad",
+    price: 5000,
     category: "Mujer",
-    pictureUrl: "https://picsum.photos/200/300?random=2",
+    pictureUrl: "https://acdn.mitiendanube.com/stores/001/053/068/products/mujer-afro-11-da492bf8650e3e52e116645715620104-640-0.jpg",
   },
 
     {
     id: 3,
-    title: "Jeans ajustados",
-    description: "Jeans de mezclilla ajustados y cómodos",
-    price: 4999,
+    title: "Lentes RayBan",
+    description: "Lentes unicos, de excelente calidad y con detalles unicos, para ver una realidad distina",
+    price: 49500,
     category: "Accesorios",
-    pictureUrl: "https://picsum.photos/200/300?random=3",
+    pictureUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCgsF_xQ5U_-PpicUgy-IuDYijxE38N5sBZw&usqp=CAU",
   },  
 ];
